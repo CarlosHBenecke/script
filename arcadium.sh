7faa3ec3d3326c54d2d02356e4c63646ea26e67b
CarlosHBenecke <carlos.benecke@sptech.school>
2023-11-21 20:52:55 -0300

    add docker

--- arcadium.sh
+++ arcadium.sh
@@ -13,10 +13,18 @@ docker --version
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo docker pull mysql:5.7
-#configurar banco antes de iniciar o java para inserir no banco de dados
+docker-compose --version 
+
+if [ $? = 0 ]; 
+    then 
+        echo “docker-compose instalado”
+    else 
+        sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
+fi
+sudo docker-compose up
+sudo docker start bdArcadium
 mkdir arcadium
-wget "https://github.com/Arcadium-com/Foodie-Kioskie-Jar/blob/main/foodie-kioskie.jar" "/home/ubuntu/arcadium"
-# git clone "https://github.com/Arcadium-com/Foodie-Kioskie-Jar.git" "/home/ubuntu/arcadium"
+git clone "https://github.com/Arcadium-com/Foodie-Kioskie-Jar.git" "/home/ubuntu/arcadium"
 cd /home/ubuntu/arcadium
 java -jar foodie-kioskie.jar
 
